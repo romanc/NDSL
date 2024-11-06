55c3bfdaf928af53bcc9a121c0d009b457ba3900
Roman Cattaneo <>
2024-11-06 16:46:48 +0100

    second, not so naive version
    greedily merge consecutive IJ loops and K loop if you can

--- testfile_schedule-tree.py
+++ testfile_schedule-tree.py
@@ -4,6 +4,7 @@ from gt4py.cartesian.gtscript import (
     computation,
     interval,
     PARALLEL,
+    FORWARD
 )
 from ndsl.boilerplate import get_factories_single_tile_orchestrated_cpu
 from ndsl.constants import X_DIM, Y_DIM, Z_DIM
@@ -21,7 +22,7 @@ stencil_factory, ijk_quantity_factory = get_factories_single_tile_orchestrated_c
     domain[0], domain[1], domain[2], 0
 )
 
-
+# simple case (working)
 def double_map(in_field: FloatField, out_field: FloatField):
     with computation(PARALLEL), interval(...):
         out_field = in_field
@@ -29,6 +30,60 @@ def double_map(in_field: FloatField, out_field: FloatField):
     with computation(PARALLEL), interval(...):
         out_field = in_field * 3
 
+# simple case of (currently) non-mergeable intervals (working)
+def double_map_with_different_interval(in_field: FloatField, out_field: FloatField):
+    with computation(PARALLEL), interval(1, None):
+        out_field = in_field
+
+    with computation(PARALLEL), interval(...):
+        out_field = in_field * 3
+
+# this should not merge (size doesn't match)
+# somehow this throws an error
+# def double_map(in_field: FloatField, out_field: FloatField):
+#     with computation(PARALLEL), interval(...):
+#         tmp = in_field * 3
+# 
+#     with computation(PARALLEL), interval(...):
+#         out_field = tmp[1, 0, 0] + in_field
+
+# this is fine (working)
+def loop_and_map(in_field: FloatField, out_field: FloatField):
+    with computation(FORWARD), interval(...):
+        out_field = in_field
+
+    with computation(PARALLEL), interval(...):
+        out_field = in_field * 3
+
+# no overcomputation (yet) (working)
+def overcomputation_guard_with_if(in_field: FloatField, out_field: FloatField):
+    with computation(PARALLEL), interval(1, None):
+        out_field = in_field
+
+    with computation(PARALLEL), interval(...):
+        out_field = in_field * 3
+
+# merging IJ - keeping K loops (working)
+def mergable_or_not(in_field: FloatField, out_field: FloatField):
+    with computation(PARALLEL), interval(1, None):
+        out_field = in_field
+
+    with computation(PARALLEL), interval(...):
+        out_field = in_field * 3
+
+    with computation(PARALLEL), interval(1, None):
+        out_field = in_field * 4
+
+# merging IJ - keeping K loops (working)
+def not_mergeable(in_field: FloatField, out_field: FloatField):
+    with computation(PARALLEL), interval(1, None):
+        out_field = in_field
+
+    with computation(PARALLEL), interval(...):
+        out_field = in_field * 3
+
+    with computation(PARALLEL), interval(1, None):
+        in_field = out_field
 
 class DaCeGT4Py_Bridge:
     def __init__(self, stencil_factory: StencilFactory):
@@ -41,8 +96,8 @@ class DaCeGT4Py_Bridge:
     def __call__(self, in_field: FloatField, out_field: FloatField):
         self.stencil(in_field, out_field)
 
-
-class MapMerge(dace_stree.ScheduleNodeTransformer):
+# first (naive (and broken)) version
+class MapMergeNaive(dace_stree.ScheduleNodeTransformer):
     def _merge_maps(self, children: dace_stree.ScheduleTreeScope | dace_stree.MapScope):
         map_scopes = [
             map_scope for map_scope in children if isinstance(map_scope, dace_stree.MapScope)
@@ -50,7 +105,6 @@ class MapMerge(dace_stree.ScheduleNodeTransformer):
 
         if len(map_scopes) == 0:
             # stop the recursion if there's no more maps to consider
-            # return
             return children
         
         if len(map_scopes) == 1:
@@ -58,35 +112,32 @@ class MapMerge(dace_stree.ScheduleNodeTransformer):
             map_index = children.index(map_scope)
 
             # recurse deeper, see if we can merge more maps
-            # return self._merge_maps(children[map_index].children)
             children[map_index].children = self._merge_maps(map_scope.children)
             return children
 
-        # TODO
         # We have at least two maps at this level. Attempt to merge them.
         i = 0
         while i < len(children):
-            # first_map = children[i]
-            if not isinstance(children[i], dace_stree.MapScope):
+            first_map = children[i]
+            if not isinstance(first_map, dace_stree.MapScope):
                 i = i + 1
                 continue
             
             j = i + 1
             while j < len(children):
-                # second_map = children[j]
+                second_map = children[j]
 
-                if not isinstance(children[j], dace_stree.MapScope):
+                if not isinstance(second_map, dace_stree.MapScope):
                     j = j + 1
                     continue
 
-                if children[i].node.range == children[j].node.range:
-                    print(f"merging {children[i].node.range} with {children[j].node.range}")
-                    children[i].children.extend(children[j].children)
+                if first_map.node.range == second_map.node.range:
+                    print(f"merging {first_map.node.range} with {second_map.node.range}")
+                    first_map.children.extend(second_map.children)
                     del children[j]
-                    # children[i].parent.children.remove(children[j])
                     print(f"i={i}, j={j}, len(children)={len(children)}")
                 else:
-                    print(f"not merging {children[i].node.range} with {children[j].node.range}")
+                    print(f"not merging {first_map.node.range} with {second_map.node.range}")
 
                 j = j + 1
             i = i + 1
@@ -95,6 +146,66 @@ class MapMerge(dace_stree.ScheduleNodeTransformer):
 
     def visit_ScheduleTreeScope(self, node: dace_stree.ScheduleTreeScope):
         node.children = self._merge_maps(node.children)
+
+class MapMerge(dace_stree.ScheduleNodeTransformer):
+    def _merge_maps(self, children: List[dace_stree.ScheduleTreeNode]):
+        # count number of maps in children
+        map_scopes = [
+            map_scope for map_scope in children if isinstance(map_scope, dace_stree.MapScope)
+        ]
+
+        if len(map_scopes) == 0:
+            # stop the recursion
+            return children
+
+        if len(map_scopes) == 1:
+            map_scope = map_scopes[0]
+            map_index = children.index(map_scope)
+
+            # recurse deeper, see if we can merge more maps
+            children[map_index].children = self._merge_maps(map_scope.children)
+            return children
+
+        # We have at least two maps at this level. Attempt to merge consecutive maps
+        i = 0
+        while i < len(children):
+            first_map = children[i]
+            # skip all non-maps
+            if not isinstance(first_map, dace_stree.MapScope):
+                i += 1
+                continue
+
+            j = i + 1
+            while j < len(children):
+                second_map = children[j]
+
+                # skip all non-maps
+                if not isinstance(second_map, dace_stree.MapScope):
+                    j += 1
+                    continue
+
+                mergeable = first_map.node.range == second_map.node.range
+                if mergeable:
+                    # merge, recurse into children
+                    first_map.children.extend(second_map.children)
+                    del children[j]
+                    first_map.children = self._merge_maps(first_map.children)
+                else:
+                    # we couldn't merge, try the next consecutive pair
+                    i += 1
+
+                # break out of the inner while loop
+                break
+
+            # in case we merged everything ...
+            if j >= len(children):
+                i += 1
+
+        return children
+
+    def visit_ScheduleTreeScope(self, node: dace_stree.ScheduleTreeScope):
+        node.children = self._merge_maps(node.children)
+
 
 if __name__ == "__main__":
     I = np.arange(domain[0] * domain[1] * domain[2], dtype=np.float64).reshape(domain)
