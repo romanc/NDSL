07bf079472fc4d8fcea79cb6cbc617697b12fa93
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-10-28 10:15:01 +0100

    undo "__all__" changes

--- ndsl/quantity/__init__.py
+++ ndsl/quantity/__init__.py
@@ -5,10 +5,4 @@ from .state import State
 
 from .local import Local  # isort: skip
 
-__all__ = [
-    "Local",
-    "Quantity",
-    "QuantityMetadata",
-    "QuantityHaloSpec",
-    "State",
-]
+__all__ = ["Local", "Quantity", "QuantityMetadata", "QuantityHaloSpec", "State"]
